dab1198b5e0372eddbd037824cda6b5b09367c29
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-07-24 02:26:17 +0530

    push

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ import messageRouter from './routes/messageRoutes';
 
 
 const corsOptions = {
-  origin: "*",
+  origin:process.env.FRONTEND_URL,
   credentials: true,
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
